8531788f22dcf8de3e61022fbca8d79f508aa699
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-20 16:16:24 +0200

    Add exc4_areThereDuplicates solution

---
+++ Amir/exc4_areThereDuplicates.js
@@ -0,0 +1,21 @@
+//Time - O(n)
+//Space - O(n)
+function areThereDuplicatesTime(...args) {
+  const lut = {};
+  for (let arg of args) {
+    if (!lut[arg]) lut[arg] = 1;
+    else return true;
+  }
+  return false;
+}
+
+//Time - O(n log n)
+//Space - O(1)
+function areThereDuplicatesSpace(...args) {
+  args.sort();
+  for (let i = 0; i < args.length - 1; i++) {
+    if (args[i] === args[i + 1]) return true;
+  }
+  return false;
+}
+console.log(areThereDuplicates(4, 2, 3, 4));
