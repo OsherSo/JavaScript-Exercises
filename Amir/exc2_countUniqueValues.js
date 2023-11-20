be0c2a7bcb8b6c90ae86174865f384080f17d7be
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-20 16:15:03 +0200

    Add exc4_areThereDuplicates solution

--- Amir/exc2_countUniqueValues.js
+++ Amir/exc2_countUniqueValues.js
@@ -1,15 +1,14 @@
 function countUniqueValues(sortedArr) {
-    if (sortedArr.length === 0) return 0
-  
-    let i = 0;
-    let j = 1;
-    while (j < sortedArr.length) {
-      if (sortedArr[j] !== sortedArr[i]) {
-        sortedArr[++i] = sortedArr[j];
-      }
-      ++j;
+  if (sortedArr.length === 0) return 0;
+
+  let i = 0;
+  let j = 1;
+  while (j < sortedArr.length) {
+    if (sortedArr[j] !== sortedArr[i]) {
+      sortedArr[++i] = sortedArr[j];
     }
-  
-    return i + 1;
+    ++j;
   }
-  
+
+  return i + 1;
+}
