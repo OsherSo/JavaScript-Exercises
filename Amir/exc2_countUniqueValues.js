24ea2f5d5de87e8574c89d5a16a7afe668e03a00
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-20 15:33:54 +0200

    Add exc3_sameFrequency solution

---
+++ Amir/exc2_countUniqueValues.js
@@ -0,0 +1,15 @@
+function countUniqueValues(sortedArr) {
+    if (sortedArr.length === 0) return 0
+  
+    let i = 0;
+    let j = 1;
+    while (j < sortedArr.length) {
+      if (sortedArr[j] !== sortedArr[i]) {
+        sortedArr[++i] = sortedArr[j];
+      }
+      ++j;
+    }
+  
+    return i + 1;
+  }
+  
