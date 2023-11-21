88dbebfd686f036d36fd73176ec109571f5b275a
Osher Solimany <solimani.osher@gmail.com>
2023-11-21 11:37:24 +0200

    Change Exercise 14 solution

--- Osher/Recursion/14_fib.js
+++ Osher/Recursion/14_fib.js
@@ -3,8 +3,7 @@ Write a recursive function called fib which accepts a number and returns the nth
 Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
 */
 
-function fib(index) {
-  if (index === 1) return 1;
-  if (index === 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
 }
